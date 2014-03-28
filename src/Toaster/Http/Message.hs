a1fa9ef24baf720b3d9545e51436ae2430a04255
Nick Hibberd <nhibberd@gmail.com>
2014-03-28 16:04:30 +1000

    Increasing paging limit

--- src/Toaster/Http/Message.hs
+++ src/Toaster/Http/Message.hs
@@ -34,11 +34,11 @@ create c d =
 
 retrieveInit :: Connection -> IO [Message]
 retrieveInit c =
-  withTransaction c $ query_ c "SELECT id, message FROM messages ORDER BY id DESC LIMIT 15"
+  withTransaction c $ query_ c "SELECT id, message FROM messages ORDER BY id DESC LIMIT 20"
 
 retrieve :: Int -> Connection -> IO [Message]
 retrieve i c =
-  withTransaction c $ query c "SELECT id, message FROM messages where id<(?) ORDER BY id DESC LIMIT 30" (Only i)
+  withTransaction c $ query c "SELECT id, message FROM messages where id<(?) ORDER BY id DESC LIMIT 15" (Only i)
 
 retrieveSince :: Int -> Connection -> IO [Message]
 retrieveSince i c =
