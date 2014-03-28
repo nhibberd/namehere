1f9f578e13cd9eb64f5e74fe6e645825100abce8
Nick Hibberd <nhibberd@gmail.com>
2014-03-28 14:50:21 +1000

    Polling

--- web/js/controller.js
+++ web/js/controller.js
@@ -2,16 +2,27 @@
 
 var toaster = angular.module('toaster', [ 'ui.bootstrap' ]);
 
-toaster.controller('ChatController', function($scope, $http) {
+toaster.controller('ChatController', function($scope, $http, $timeout) {
 
-	$http.get('messages').success(function(data) {
-		$scope.messages = data;
-	});
+    var id = 0;
+    $scope.messages = [];
+
+    var poll = function() {
+        $timeout(function() {
+            $http.get('newmessages?id=' + id).success(function(data) {
+                data.forEach(function(entry) {
+                    $scope.messages.unshift({ message: entry.message })
+                    id = Math.max(id,entry.id);
+                });
+                poll();
+            });
+        }, 1000);
+    };     
+    poll();
 
 	$scope.submit = function(msg) {
-		$scope.messages.unshift({ message: msg.message })
 		$http.post('message', msg).success(function(data) {
-
+          // do nothing
 		});
 	};
 
@@ -19,10 +30,5 @@ toaster.controller('ChatController', function($scope, $http) {
         $http.get('messages/' + msg.id).success(function(data) {
         
         }); 
-    };
-
-	function poll (msg) {
-        $http.get('blah?id=' + msg.id).success(function(data) {
-        });
     };
 });
